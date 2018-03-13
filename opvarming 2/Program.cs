68489a05a748843b287eb3b031f23c337612acda
benjamin ostner <benjamin110101@gmail.com>
2018-03-13 13:43:21 +0100

    done

--- opvarming 2/Program.cs
+++ opvarming 2/Program.cs
@@ -8,8 +8,39 @@ namespace opvarming_2
 {
     class Program
     {
+        static int GetIntFromUser(string spørgsmål)
+        {
+            Console.Write(spørgsmål);
+            string input;
+            int resultat;
+            input = Console.ReadLine();
+
+            while(! int.TryParse(input, out resultat))
+            {
+                Console.Write("Fejl, prøv igen");
+                input = Console.ReadLine();
+            }
+            return resultat;
+        }
         static void Main(string[] args)
         {
+            int tal1, tal2;
+            double resultat;
+
+
+
+            tal1 = GetIntFromUser("Indtast et heltal: ");
+            tal2 = GetIntFromUser("Indats et heltal, foskelligt fra nul: ");
+
+            while (tal2 == 0)
+            {
+                tal2 = GetIntFromUser("Indtast et heltal, forskelligt fra nul: ");
+            }
+
+            resultat = (double)tal1 / (double)tal2;
+
+            Console.WriteLine("Første tal divideret med andet tal er : " + resultat);
+            Console.ReadKey();
         }
     }
 }
